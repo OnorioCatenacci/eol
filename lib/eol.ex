07fec665f158b9cde0dae83cbb921d755dc3816d
Onorio Catenacci <OnorioCatenacci@quickenloans.com>
2014-07-21 15:30:14 -0400

    Adding tests with a good connection string and a bad one

--- lib/eol.ex
+++ lib/eol.ex
@@ -1,2 +1,12 @@
 defmodule Eol do
+
+  def start(), do: :odbc.start()
+
+  def connect(driver_name, server_name, database_name, windows_authentication) do
+    connection_string = 'Driver=#{driver_name};Server=#{server_name};Database=#{database_name};Trusted_Connection=#{windows_authentication};'
+    :odbc.connect(connection_string,[])
+  end
+
+  def stop(), do: :odbc.stop()
+
 end
--- test/eol_test.exs
+++ test/eol_test.exs
@@ -1,7 +1,19 @@
 defmodule EolTest do
   use ExUnit.Case
+    @driver_name "{SQL Server}"
+    @server_name "(local)"
+    @database_name "TNM"
+    @windows_authentication true 
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "An attempt to connect to a non-existent server returns an error" do
+    Eol.start
+    assert {:error, _} = Eol.connect("#{@driver_name}","ServerDoesNotExist","#{@database_name}","#{@windows_authentication}")
+    Eol.stop
+  end
+
+  test "An attempt to connect to a good server should return ok" do
+    Eol.start
+    assert {:ok, _} = Eol.connect("#{@driver_name}","#{@server_name}","#{@database_name}","#{@windows_authentication}")
+    Eol.stop
   end
 end
